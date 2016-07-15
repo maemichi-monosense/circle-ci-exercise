383de7778c606b6fce08c309821801e1ed65ce37
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-15 19:11:12 +0900

    Add mysqli object

--- src/app/increment.php
+++ src/app/increment.php
@@ -7,11 +7,29 @@ namespace UnitTest\Sample;
  */
 class Increment
 {
+    const Host = "localhost";
+    const User = "ubuntu";
+    const Pass = '';
+    const DBName = "circle_test";
+
+    private $link;
+
     /**
      * initializer
      */
     public function __construct()
     {
-        return;
+        $this->link = new mysqli(self::Host, self::User, self::Pass, self::DBName);
+        if ($this->mysqli->connect_error) {
+            echo $this->mysqli->connect_error;
+            exit;
+        } else {
+            $this->mysqli->set_charset("utf8");
+        }
+    }
+
+    function __destruct()
+    {
+        $this->mysqli->close();
     }
 }
