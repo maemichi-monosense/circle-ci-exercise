34d3b8c7d9d86da45e7ea3228f0844165cc3af63
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-15 18:44:36 +0900

    Add skeleton for PHP TDD

---
+++ src/app/increment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UnitTest\Sample;
+
+/**
+ * Increment a value in MySQL
+ */
+class Increment
+{
+    /**
+     * initializer
+     */
+    public function __construct()
+    {
+        return;
+    }
+}
