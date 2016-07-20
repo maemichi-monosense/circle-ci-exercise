418a25415389b4dd5aca801d29e06f3e27ebba98
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-20 12:20:24 +0900

    Fix PDO for test

--- test/app/AtomicCounterTest.php
+++ test/app/AtomicCounterTest.php
@@ -5,16 +5,12 @@ namespace UnitTest\Sample;
 class AtomicCounterTest extends \PHPUnit_Framework_TestCase
 {
     // Object to be tested
-    private $target;
-    // Mocks
-    private $statement;
-    private $connection;
-    // PDO
-    private $pdo;
+    public $target;
+    public $pdo;
 
     public function setUp()
     {
         $this->target = new AtomicCounter();
-        $this->pdo = new \PDO('mysql:dbname=test;');
+        $this->pdo    = clone TestUtil::snatch_property($this->target, 'pdo');
     }
 }
---
+++ test/app/TestUtil.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yuya
+ * Date: 2016/07/20
+ * Time: 12:17
+ */
+
+namespace UnitTest\Sample;
+
+
+class TestUtil
+{
+    /**
+     * @see \ReflectionClass
+     * @see \ReflectionProperty
+     *
+     * @param object|string $object
+     * @param string        $name
+     *
+     * @return mixed
+     */
+    public static function snatch_property($object, $name)
+    {
+        $ref_class = new \ReflectionClass($object);
+        return $ref_class->getProperty($name)->getValue();
+    }
+}
