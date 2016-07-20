098c667b2a447cbee2e69bbb3ef2365e2569528e
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-20 14:57:34 +0900

    Add test case

--- test/app/AtomicCounterTest.php
+++ test/app/AtomicCounterTest.php
@@ -8,12 +8,25 @@ use UnitTest\Sample\App\AtomicCounter;
 class AtomicCounterTest extends TestCase
 {
     // Object to be tested
+    /**
+     * @var AtomicCounter
+     */
     public $target;
-    public $pdo;
+    /**
+     * @var \PDO
+     */
+    private $pdo;
 
     public function setUp()
     {
         $this->target = new AtomicCounter();
         $this->pdo    = clone TestUtil::snatch_property($this->target, 'pdo');
+    }
+
+    public function testCountUp($id = 1)
+    {
+        $init_count = $this->target->get_count($id);
+        $new_count  = $this->target->count_up($id);
+        $this->assertEquals($init_count + 1, $new_count, 'The count should be incremented.');
     }
 }
