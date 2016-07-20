4387e75613e8eaa82384dff563d5fe99628bb789
yuya-m-ms <maemichi@monosense.co.jp>
2016-07-20 11:55:54 +0900

    Fix SQL query

--- test/db/data.sql
+++ test/db/data.sql
@@ -1,6 +1,1 @@
-CREATE TABLE test.atomic_counter (
-    id SERIAL PRIMARY KEY
-  , count INT UNSIGNED DEFAULT 0
-);
-
 INSERT INTO test.atomic_counter(count) VALUES (3);
--- test/db/setup.sql
+++ test/db/setup.sql
@@ -1,1 +1,6 @@
 CREATE DATABASE test;
+
+CREATE TABLE test.atomic_counter (
+    id SERIAL PRIMARY KEY
+  , count INT UNSIGNED DEFAULT 0
+);
